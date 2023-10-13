9f962b181a562851b9ed0ba2ce482bd5bc926798
RPatroch <reza0000835@gmail.com>
2023-10-13 08:48:16 +0330

    Add Response Model In Api Layer

---
+++ PatrochBlog.Api/ResponseModel/Response.cs
@@ -0,0 +1,14 @@
+﻿namespace PatrochBlog.Api.ResponseModel
+{
+    public class Response<T>
+    {
+        public T? Data { get; set; } 
+        public string ErrorMessage { get; set; } = string.Empty;
+        public bool Success { get; set; }
+
+        public Response(bool isSuccess) 
+        {
+            Success = isSuccess;
+        }
+    }
+}
