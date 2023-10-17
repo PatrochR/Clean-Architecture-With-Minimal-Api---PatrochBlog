3a5dff844bc4f52240efe5f52c07040823c34cb9
RPatroch <reza0000835@gmail.com>
2023-10-17 14:55:26 +0330

    Add Interface For Tag Repository

---
+++ PatrochBlog.Core/Interfaces/Repository/ITagRepository.cs
@@ -0,0 +1,19 @@
+﻿using PatrochBlog.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatrochBlog.Core.Interfaces.Repository
+{
+    public interface ITagRepository
+    {
+        Task<List<Tag>> GetAllTags();
+        Task<Tag> GetTagById(int id);    
+        Task<int> CreateTag(Tag tag);
+        Task EditTag(Tag tag);
+        Task DeleteTag(int id);
+
+    }
+}
