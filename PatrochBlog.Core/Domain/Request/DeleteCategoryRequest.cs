9a960969c76571616295e157387ddaff9b1af78e
RPatroch <reza0000835@gmail.com>
2023-10-17 14:54:55 +0330

    Add Delete Request for Category

---
+++ PatrochBlog.Core/Domain/Request/DeleteCategoryRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatrochBlog.Core.Domain.Request
+{
+    public class DeleteCategoryRequest
+    {
+        public int Id { get; set; }
+    }
+}
